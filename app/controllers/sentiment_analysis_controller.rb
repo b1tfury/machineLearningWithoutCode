528f051f72f7b48922515d29ba5051425101b123
sahil <sahil.kharb@go-jek.com>
2016-01-30 22:17:46 +0530

    shit

--- app/controllers/sentiment_analysis_controller.rb
+++ app/controllers/sentiment_analysis_controller.rb
@@ -1,7 +1,5 @@
 class SentimentAnalysisController < ActionController::Base
   def create
-    p '8'*100
-    p params.inspect
     File.open("training.csv", "wb") { |f| f.write(training_data.read) }
     File.open("test.csv", "wb") { |f| f.write(test_data.read) }
     sample_model = `python try-tf-master/softmax.py --train training.csv --test test.csv --num_epochs 5 --verbose True`
--- env.sh
+++ env.sh
@@ -1,3 +1,37 @@
+#!/usr/bin/env bash
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get install -y nodejs
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+rbenv install 2.2.3
+rbenv global 2.2.3
+ruby -v
+gem install bundle
+sudo apt-get install nginx
+: '
+ change the following file /etc/nginx/sites-enabled/default
+                proxy_pass http://127.0.0.1:3000;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $http_x_forwarded_for;
+                proxy_send_timeout  120;
+                proxy_read_timeout 120;
+                proxy_connect_timeout 120;
+                send_timeout  120;
+
+                after this
+                sudo service nginx restart
+'
+bundle install
+
 sudo apt-get install -y python-pip python-dev
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
 sudo pip install numpy
+rails server -e production
